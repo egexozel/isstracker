7400fbe885507c0215057d9ab085b5e1c2b7ae51
Ege Özel <egexozel@gmail.com>
2023-06-26 00:00:12 +0300

    t

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}",
-            "./index.html"],
+            "./index.html",
+            "./index.js"],
   theme: {
     extend: {},
   },
